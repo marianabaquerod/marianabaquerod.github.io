019ec02459d79d064ad1b78ba24821d5c7265535
Mariana Baquero <mbaquerodegwitz@gmail.com>
2022-02-15 14:05:08 -0500

    fix(): hello

--- src/Components/NavigationBar/NavigationBarProjects.js
+++ src/Components/NavigationBar/NavigationBarProjects.js
@@ -1,12 +1,9 @@
 import "./NavigationBar.css";
 import * as React from "react";
-import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
-import Typography from "@mui/material/Typography";
 import IconButton from "@mui/material/IconButton";
 import MenuIcon from "@mui/icons-material/Menu";
-import { ButtonGroup } from "@mui/material";
 import logo from "../../Images/mbd.png";
 import Divider from "@mui/material/Divider";
 import Drawer from "@mui/material/Drawer";
